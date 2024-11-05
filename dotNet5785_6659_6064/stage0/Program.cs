eb3752690eee7ded9253bfffbe8d66c19e03bd17
GuedaliaMahonAccount <gsebbah@g.jct.ac.il>
2024-11-05 17:33:27 +0200

    first code

--- dotNet5785_6659_6064/stage0/Program.cs
+++ dotNet5785_6659_6064/stage0/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Targil0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name:");
+
+            // Read the user's name from the console
+            string name = Console.ReadLine();
+
+            // Display the welcome message with the user's name
+            Console.WriteLine("{0}, welcome to my first console application!", name);
+        }
+    }
+}
