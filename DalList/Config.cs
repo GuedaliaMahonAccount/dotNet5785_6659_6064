42d11a78272bcf56b735dc122ffe060f8e4cf0cb
נועם חדד <nohadad@g.jct.ac.il>
2024-11-12 18:27:10 +0200

    Config

---
+++ DalList/Config.cs
@@ -0,0 +1,21 @@
+﻿namespace Dal
+{
+    internal static class Config
+    {
+        const int startCallId = 1;
+        static int nextCallId = startCallId;
+        internal static int NextCallId { get => nextCallId++; }
+        const int startAssignmentId = 100;
+        static int nextAssignmentId = startAssignmentId;
+        internal static int NextAssignmentId { get => nextAssignmentId++; }
+        internal static DateTime Clock { get; set; } = DateTime.Now;
+        internal static TimeSpan RiskRange { get; set; } = TimeSpan.FromHours(4);
+        internal static void Reset()
+        {
+            nextCallId = startCallId;
+            nextAssignmentId = startAssignmentId;
+            Clock = DateTime.Now;
+            RiskRange = TimeSpan.FromHours(4);
+        }
+    }
+}
