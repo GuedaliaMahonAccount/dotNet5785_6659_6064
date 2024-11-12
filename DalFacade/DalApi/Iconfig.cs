5e1d760f5aa29124a233696d32cbe8508caf15b3
GuedaliaMahonAccount <gsebbah@g.jct.ac.il>
2024-11-12 18:21:10 +0200

    add i config

---
+++ DalFacade/DalApi/Iconfig.cs
@@ -0,0 +1,16 @@
+﻿
+using DO;
+
+namespace DalApi
+{
+
+    internal interface Iconfig
+    {   void Create(Config item); //Creates new entity object in DAL
+        Config? Read(int id); //Reads entity object by its ID 
+        List<Config> ReadAll(); //stage 1 only, Reads all entity objects
+        void Update(Config item); //Updates entity object
+        void Delete(int id); //Deletes an object by its Id
+        void DeleteAll(); //Delete all entity objects
+
+    }
+}
