ff14aa7c7663d41b3fa1a765a336f5eccd48c0fa
Guedalia Sebbah <gsebbah@g.jct.ac.il>
2024-11-12 21:42:26 +0200

    chapter 8

--- DalFacade/DalApi/Iconfig.cs
+++ DalFacade/DalApi/Iconfig.cs
@@ -1,7 +1,14 @@
 ﻿
 namespace DalApi;
 
+/// <summary>
+/// Config class to manage unique ID generation, system clock, and risk range settings.
+/// </summary>
 public interface IConfig
-{   
+{
+    int NextCallId { get; }
+    int NextAssignmentId { get; }
+    DateTime Clock { get; set; }
+    TimeSpan RiskRange { get; set; }
 
 }
---
+++ DalList/ConfigImplementation.cs
@@ -0,0 +1,37 @@
+﻿namespace Dal
+{
+    using DalApi;
+
+    /// <summary>
+    /// Implementation of IConfig interface, delegating properties and methods to the Config class.
+    /// </summary>
+    public class ConfigImplementation : IConfig
+    {
+
+        // Gets the next unique call ID.
+        public int NextCallId => Config.NextCallId;
+
+        // Gets the next unique assignment ID.
+        public int NextAssignmentId => Config.NextAssignmentId;
+
+        // Gets or sets the system clock representing the current time in the application.
+        public DateTime Clock
+        {
+            get => Config.Clock;
+            set => Config.Clock = value;
+        }
+
+        // Gets or sets the time range for risk assessment operations.
+        public TimeSpan RiskRange
+        {
+            get => Config.RiskRange;
+            set => Config.RiskRange = value;
+        }
+
+        // Resets all configuration variables to their initial values.
+        public void Reset()
+        {
+            Config.Reset();
+        }
+    }
+}
