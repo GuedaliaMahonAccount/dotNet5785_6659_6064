c82a34bc3064574f89b8c7a85a84a32dd249b29b
Guedalia Sebbah <gsebbah@g.jct.ac.il>
2024-11-12 20:41:51 +0200

    remake empty iconfig

--- DalFacade/DalApi/Iconfig.cs
+++ DalFacade/DalApi/Iconfig.cs
@@ -1,14 +1,7 @@
 ﻿
 namespace DalApi;
 
-using 
-
-public interface Iconfig
-{   void Create(Config item); //Creates new entity object in DAL
-    Config? Read(int id); //Reads entity object by its ID 
-    List<Config> ReadAll(); //stage 1 only, Reads all entity objects
-    void Update(Config item); //Updates entity object
-    void Delete(int id); //Deletes an object by its Id
-    void DeleteAll(); //Delete all entity objects
+public interface IConfig
+{   
 
 }
