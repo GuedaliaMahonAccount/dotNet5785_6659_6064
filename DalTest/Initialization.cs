59eabfb675e882f18898b7074341f77249703e95
Guedalia Sebbah <gsebbah@g.jct.ac.il>
2024-11-13 20:43:51 +0200

    create call is good

--- DalTest/Initialization.cs
+++ DalTest/Initialization.cs
@@ -29,6 +29,7 @@ public static class Initialization
         "motibendavid890@outlook.com", "yaakovperetz123@gmail.com", "ruthazulay456@gmail.com",
         "itzikshalev789@outlook.com", "sarabar234@walla.com", "yonatanezra567@gmail.com"
     };
+
         // Create a strong password
         string password = "";
         string upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -99,6 +100,7 @@ public static class Initialization
 
     private static void CreateCalls()
     {
+        // creat array of each sade of call
         string[] descriptions = new[]
 {
     "Preparing nutritious meals for soldiers stationed at remote bases.",
@@ -207,71 +209,86 @@ public static class Initialization
     CallType.DeliverSupplies,           // Delivering donated blankets and winter gear for soldiers in cold areas.
     CallType.OrganizeRecreationalEvent, // Organizing group therapy sessions to support soldier well-being.
 };
-        string[] addresses = new[]
-        {
-    "Ino Shaki 6",
-    "Perets Bernstein 2",
-    "Ben Yehuda 13",
-    "Dizengoff 99",
-    "Herzl 45",
-    "Weizmann 17",
-    "Jabotinsky 35",
-    "King George 20",
-    "Rothschild 16",
-    "Aluf David Elazar 10",
-    "Sokolov 22",
-    "Yafo 32",
-    "Allenby 72",
-    "Hillel Yaffe 3",
-    "Rabbi Akiva 5",
-    "Ben Gurion 50",
-    "Yigal Alon 98",
-    "Keren Hayesod 21",
-    "Hertzel 8",
-    "Shaul Hamelech 22",
-    "Menachem Begin 6",
-    "Nahalat Binyamin 25",
-    "Bialik 15",
-    "Sderot Yerushalayim 10",
-    "Shenkar 12",
-    "David HaMelech 7",
-    "Moshe Dayan 33",
-    "Hankin 40",
-    "Balfour 28",
-    "Levi Eshkol 4",
-    "Herzl 15",
-    "Hagolan 12",
-    "HaPalmach 2",
-    "HaHistadrut 20",
-    "HaYarkon 200",
-    "Ruppin 19",
-    "HaAliyah 11",
-    "Neot Peres 7",
-    "HaMasger 17",
-    "Mish'ol HaNesharim 5",
-    "Rabbi Meir 3",
-    "Herzl 89",
-    "Pinsker 10",
-    "HaShomer 3",
-    "Nili 8",
-    "Sderot Rothschild 9",
-    "Trumpeldor 7",
-    "Bergen Street 4",
-    "Even Gvirol 19",
-    "Giora Yoseftal 2",
-    "Ben Tzvi 5"
+        string[] addresses = new[] {
+    "גרנדר קניון, David Tuviyahu Ave 125, Be'er Sheva",
+    "דרך אליהו נאוי 28, Beersheba",
+    "מתחם תחנת הדלק אלון, 31, Arad",
+    "Mivtsa Nakhshon Street 60, Be'er Sheva",
+    "Yitzhack I. Rager Boulevard 24, Be'er Sheva",
+    "Tsvi Bornstein Street 312, Yeruham",
+    "איזור תעשייה, הפועלים 5, דימונה",
+    "Herzl Street 43, Be'er Sheva",
+    "Derekh Hebron 21, Beersheba",
+    "Derekh Hebron 4, Beersheba",
+    "Kornmehl Farm M.E. Ramat Negev D.N. Halutza",
+    "רסקו סנטר, HaTikva 8, Beersheba",
+    "40, Tlalim",
+    "קניון פרץ סנטר, דימונה",
+    "פרץ סנטר - אזור תעשיה דרומי, Dimona",
+    "Tsvi Taub Street 7, Be'er Sheva",
+    "Ha-Bdil Street 17, ב״ש",
+    "ברוך קטינקא 2 ישפרו פלאנט, Beersheba",
+    "MALL-7, הע\"ל, Be'er Sheva",
+    "Kfar Hadarom Street 1, Be'er Sheva",
+    "Kakal Street 55-79, Be'er Sheva",
+    "HaAtsmaut Street 14, Be'er Sheva",
+    "Kakal Street 79, Be'er Sheva",
+    "Ha-Histadrut Street 22, Be'er Sheva",
+    "דרום, Kakal Street 78, Be'er Sheva",
+    "מתחם B7, Heil Handasa 2, Beersheba",
+    "מתחם ביג, חיל ההנדסה 1, באר שבע",
+    "Heil Handasa Street 6, Be'er Sheva",
+    "Eliyahu Nawi Street, Be'er Sheva",
+    "Kakal Street 195, Be'er Sheva",
+    "מרכז ביג, Derekh Hebron 21, Be'er Sheva",
+    "Heil Handasa Street 1, Be'er Sheva",
+    "Derekh Hebron 66, Beersheba",
+    "Herzl Street 55, Be'er Sheva",
+    "Yitshak Ben Zvi Street 4, Be'er Sheva",
+    "Yitshak Nafha Street 36 א', Be'er Sheva",
+    "קניון עזריאלי הנגב, Be'er Sheva",
+    "Eli'ezer Smoli Street 1, Be'er Sheva",
+    "Sderot Johana Jabotinsky 11, Beersheba",
+    "Yitshak Nafha Street 25, Be'er Sheva",
+    "Ha-Tikva Street 8, Be'er Sheva",
+    "Sderot Yerushalayim 2, Beersheba",
+    "תחנת דלק פז, Sderot Yerushalayim 4, Beersheba",
+    "מגדלי אביסרור, שדרות יצחק רגר 53, באר שבע",
+    "David Tuviyahu Ave 125, Beersheba",
+    "Derech Metsada 47, Beersheba",
+    "מרכז הנגב דרך מצדה 6, Beersheba"
+};
+        float[] latitudes = new float[] {
+31.2506405f, 31.2467805f, 31.2497676f, 31.2605014f, 31.2473633f, 30.98850599999999f, 31.062823f, 31.2396368f, 31.2391961f, 31.2377661f, 30.972753f, 31.2474244f, 30.9935689f, 31.0599323f, 31.0599323f, 31.22165469999999f, 31.2260083f, 31.2244175f, 31.2333045f, 31.2360614f, 31.2380911f, 31.2385546f, 31.2385105f, 31.2381019f, 31.2388346f, 31.243331f, 31.2447522f, 31.2443165f, 31.2492702f, 31.239762f, 31.2443691f, 31.2447522f, 31.244116f, 31.24109f, 31.2439462f, 31.2476115f, 31.243726f, 31.238365f, 31.238365f, 31.2386544f, 31.2461259f, 31.2474978f, 31.245456f, 31.2457355f, 31.2538143f, 31.2506405f, 31.2506405f, 31.2506405f, 31.2581979f, 31.2591166f
+};
+        float[] longitudes = new float[] {
+34.7716625f, 34.8156994f, 35.1914546f, 34.7873239f, 34.7978134f, 34.927951f, 35.0192015f, 34.7877478f, 34.7967932f, 34.7944248f, 34.7755085f, 34.79862f, 34.7643578f, 35.0203137f, 35.0203137f, 34.8001505f, 34.8096236f, 34.8014475f, 34.7965166f, 34.7881834f, 34.7912054f, 34.7924147f, 34.7906793f, 34.7875611f, 34.7907967f, 34.812547f, 34.81256610000001f, 34.8127704f, 34.8178768f, 34.7889659f, 34.8114871f, 34.81256610000001f, 34.8060877f, 34.789058f, 34.7965666f, 34.8106433f, 34.7946189f, 34.7726193f, 34.7726193f, 34.7740828f, 34.8120535f, 34.7986387f, 34.7814817f, 34.7814879f, 34.7983284f, 34.7716625f, 34.7716625f, 34.7716625f, 34.792098f, 34.7955966f
 };
 
+        //get the time of the maarachet
+        DateTime start = new DateTime(s_dalConfig.Clock.Year - 2, 1, 1);
 
-
-
-
-
-        foreach (var topic in descriptions)
+        for (int i = 0; i < descriptions.Length; i++)
         {
+            var startTime = start.AddDays(-s_rand.Next(1, 30)); // Random start time within the last month
+            DateTime? deadline = s_rand.NextDouble() < 0.5 // 50% chance of having a deadline or to get null
+                ? (DateTime?)startTime.AddHours(s_rand.Next(1, 72)) // Random deadline within 1-72 hours after startTime
+                : null;
 
+            //create the call  
+            var call = new Call
+            (
+                Id: i + 1,
+                CallType: callTypes[i],
+                Address: addresses[i],
+                Latitude: latitudes[i],
+                Longitude: longitudes[i],
+                StartTime: startTime,
+                Description: descriptions[i],
+                DeadLine: deadline
+            );
 
-            s_dalCall!.Create(new Call { });
+            s_dalCall!.Create(call);
         }
     }
 
