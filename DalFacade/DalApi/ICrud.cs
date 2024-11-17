c7e1589669e00d8fe884e092964168a7ef6f314c
Guedalia Sebbah <gsebbah@g.jct.ac.il>
2024-11-17 19:43:57 +0200

    nothing

--- DalFacade/DalApi/ICrud.cs
+++ DalFacade/DalApi/ICrud.cs
@@ -6,9 +6,8 @@ namespace DalApi
     {
         void Create(T item); //Creates new entity object in DAL
         T Read(int id); //Reads entity object from DAL
-        T? Read(Func<T, bool> filter);
-        IEnumerable<T> ReadAll(Func<T, bool>? filter = null); // stage 2, Reads all entity objects with optional filtering
-
+        T? Read(Func<T, bool> filter); // Reads entity object with optional filtering
+        IEnumerable<T> ReadAll(Func<T, bool>? filter = null); // Reads all entity objects with optional filtering
         void Update(T item); //Updates entity object in DAL
         void Delete(int id); //Deletes entity object from DAL
         void DeleteAll(); //Delete all entity objects from DAL
