c6045d86d41fe78308bec034771b917b9c4fddba
Guedalia Sebbah <gsebbah@g.jct.ac.il>
2024-11-27 20:53:28 +0200

    chapter 7 done

---
+++ DalXml/DalXml .cs
@@ -0,0 +1,21 @@
+﻿using DalApi;
+namespace Dal;
+
+sealed public class DalXml : IDal
+{
+    public IAssignment Assignment { get; } = new AssignmentImplementation();
+
+    public ICall Call { get; } = new CallImplementation();
+
+    public IVolunteer Volunteer { get; } = new VolunteerImplementation();
+
+    public IConfig Config { get; } = new ConfigImplementation();
+
+public void ResetDB()
+    {
+        Assignment.DeleteAll();
+        Call.DeleteAll();
+        Volunteer.DeleteAll();
+        Config.Reset();
+    }
+}
