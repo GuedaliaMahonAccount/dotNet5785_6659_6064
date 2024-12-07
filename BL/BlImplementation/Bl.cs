c4698cc590fed7831362fdd43f2eeed0fc27b649
GuedaliaMahonAccount <gsebbah@g.jct.ac.il>
2024-12-07 11:04:02 +0200

    add BL and finish cha[ter 9

---
+++ BL/BlImplementation/Bl.cs
@@ -0,0 +1,8 @@
+﻿namespace BlImplementation;
+using BlApi;
+internal class Bl : IBl
+{
+    public IAdmin Admin { get; } = new AdminImplementation();
+    public ICall Call { get; } = new CallImplementation();
+    public IVolunteer Volunteer { get; } = new VolunteerImplementation();
+}
