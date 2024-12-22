308410aaa3d4effcfacfb86b3a1c1f86495d6106
נועם חדד <nohadad@g.jct.ac.il>
2024-12-22 18:39:03 +0200

    Stage-5 Done

--- PL/Volunteer/VolunteerListWindow.xaml.cs
+++ PL/Volunteer/VolunteerListWindow.xaml.cs
@@ -16,56 +16,45 @@ using System.Windows.Shapes;
 
 namespace PL.Volunteer
 {
-    /// <summary>
-    /// Interaction logic for VolunteerListWindow.xaml
-    /// </summary>
     public partial class VolunteerListWindow : Window
     {
         static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
 
-
-        //public IEnumerable<BO.VolunteerInList> VolunteerList;
         public IEnumerable<BO.VolunteerInList> VolunteerList
         {
             get { return (IEnumerable<BO.VolunteerInList>)GetValue(VolunteerInListProperty); }
             set { SetValue(VolunteerInListProperty, value); }
         }
-        // Using DependencyProperty as the backing store for VolunteerList
+
         public static readonly DependencyProperty VolunteerInListProperty =
-            DependencyProperty.Register("VolunteerList", typeof(IEnumerable<BO.VolunteerInList>), typeof(VolunteerListWindow), new PropertyMetadata(null));
+            DependencyProperty.Register("VolunteerList", typeof(IEnumerable<BO.VolunteerInList>),
+            typeof(VolunteerListWindow), new PropertyMetadata(null));
 
         public BO.CallType Type { get; set; } = BO.CallType.None;
+
+        public BO.VolunteerInList? SelectedVolunteer { get; set; }
 
         public VolunteerListWindow()
         {
             InitializeComponent();
         }
 
-
-
         private void CbVolunteerTypeSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             queryVolunteerList();
         }
 
-
-
         private void queryVolunteerList()
-        {
-            VolunteerList = (Type == BO.CallType.None) ?
-                s_bl?.Volunteer.GetVolunteersList()! :
-                s_bl?.Volunteer.GetVolunteersList(isActive: null, sortByField: BO.VolunteerInListSortFields.CallType)!;
-        }
+            => VolunteerList = s_bl?.Volunteer.GetVolunteersList();
 
         private void volunteerListObserver()
             => queryVolunteerList();
+
         private void Window_Loaded(object sender, RoutedEventArgs e)
             => s_bl.Volunteer.AddObserver(volunteerListObserver);
+
         private void Window_Closed(object sender, EventArgs e)
             => s_bl.Volunteer.RemoveObserver(volunteerListObserver);
-
-
-        public BO.VolunteerInList? SelectedVolunteer { get; set; }
 
         private void lsvVolunteerList_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
@@ -76,6 +65,33 @@ namespace PL.Volunteer
         private void AddvolunteerButton_Click(object sender, RoutedEventArgs e)
         {
             new VolunteerWindow().Show();
+        }
+
+        private void DeleteVolunteer_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button && button.Tag is int volunteerId)
+            {
+                var result = MessageBox.Show("Are you sure you want to delete this volunteer?",
+                                           "Confirm Delete",
+                                           MessageBoxButton.YesNo,
+                                           MessageBoxImage.Question);
+
+                if (result == MessageBoxResult.Yes)
+                {
+                    try
+                    {
+                        s_bl.Volunteer.DeleteVolunteer(volunteerId);
+                        // No need to refresh the list manually as the Observer pattern will handle it
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Error deleting volunteer: {ex.Message}",
+                                      "Error",
+                                      MessageBoxButton.OK,
+                                      MessageBoxImage.Error);
+                    }
+                }
+            }
         }
     }
 }
