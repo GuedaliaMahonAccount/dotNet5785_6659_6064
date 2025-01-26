f72541ffa3b7852a6ff39d0e4a31e36e9a2ff5e7
GuedaliaMahonAccount <gsebbah@g.jct.ac.il>
2025-01-26 23:21:54 +0200

    history be observable

--- PL/User/HistoryCallWindow.xaml.cs
+++ PL/User/HistoryCallWindow.xaml.cs
@@ -6,12 +6,13 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Threading;
 using BO;
+using DO;
 
 namespace PL.User
 {
     public partial class HistoryCallWindow : Window, INotifyPropertyChanged
     {
-        private IEnumerable<object> _callDetails;
+        private IEnumerable<object> _callDetails; 
 
         // Static reference to business logic layer
         static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
@@ -36,17 +37,24 @@ namespace PL.User
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
+
+        private int _currentVolunteerId; // Field to store the current volunteer's ID
+
         public HistoryCallWindow(string volunteerName)
         {
             InitializeComponent();
             this.DataContext = this;
 
-            int volunteerId = s_bl.Volunteer.FindVolunteerID(volunteerName);
-            VolunteerIdTextBlock.Text = volunteerId.ToString();
+            _currentVolunteerId = s_bl.Volunteer.FindVolunteerID(volunteerName); // Store the current volunteer ID
+            VolunteerIdTextBlock.Text = _currentVolunteerId.ToString();
             VolunteerNameTextBlock.Text = volunteerName;
 
-            LoadCalls(volunteerId);
+            LoadCalls(_currentVolunteerId); // Load calls for the current volunteer
+
+            // Add observer to automatically refresh history
+            s_bl.Call.AddObserver(HistoryObserver);
         }
+
 
         /// <summary>
         /// Loads the call list for a specific volunteer into the DataGrid.
@@ -94,12 +102,25 @@ namespace PL.User
             }
         }
 
+        // Observer to refresh history calls on UI updates
+        private void HistoryObserver()
+        {
+            if (!_isUpdating)
+            {
+                Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    LoadCalls(_currentVolunteerId); // Refresh current calls on the UI thread
+                    _isUpdating = false; // Reset the flag after the operation is done
+                }));
+            }
+        }
+
         /// <summary>
         /// Filters the call list by call type.
         /// </summary>
         private void FilterByCallType()
         {
-            var callTypes = Enum.GetNames(typeof(CallType)).ToList(); // Get all call type names as strings
+            var callTypes = Enum.GetNames(typeof(BO.CallType)).ToList(); // Get all call type names as strings
 
             // Create a custom dialog to let the user choose a call type
             var dialog = new Window
@@ -147,7 +168,7 @@ namespace PL.User
                         // No filtering, display all calls
                         CallsDataGrid.ItemsSource = _callDetails;
                     }
-                    else if (Enum.TryParse(selectedCallTypeString, out CallType selectedCallType))
+                    else if (Enum.TryParse(selectedCallTypeString, out BO.CallType selectedCallType))
                     {
                         // Filter by the selected call type
                         var filteredCalls = _callDetails.Where(call => ((dynamic)call).Status == selectedCallType).ToList();
